9e7787793c65050c0263ef2d7397e8dc8295263e
Cissy030404 <127461047+Cissy030404@users.noreply.github.com>
2023-04-14 14:02:34 +0800

    Add files via upload

---
+++ Data Science.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.title('追思缅怀敬先烈，砥砺前行建新功')
+if st.checkbox('为牺牲的先烈献花'):
+    st.title('音乐欣赏')
+    if st.checkbox('欣赏"红色歌曲"'):
+      with st.container():
+        option = st.selectbox(
+           '选择一首歌曲',
+           ('黄河大合唱', '没有共产党就没有新中国','当那一天来临'))
+        '👇以下歌曲是《',option,'》'
+        if option=='黄河大合唱':
+          audio_file = open('./music/阿飞的小蝴蝶.wav', 'rb')
+          audio_bytes = audio_file.read()
+          st.audio(audio_bytes, format='audio/wav')
+        elif  option=='没有共产党就没有新中国':
+          audio_file = open('./music/夜曲.wav', 'rb')
+          audio_bytes = audio_file.read()
+          st.audio(audio_bytes, format='audio/wav')
+        elif  option=='当那一天来临':
+          audio_file = open('./music/倔强.wav', 'rb')
+          audio_bytes = audio_file.read()
+          st.audio(audio_bytes, format='audio/wav')
+    import streamlit as st
+
+     st.title('音乐欣赏')
+    if st.checkbox('重温"红色歌经典"'):
+    video_file = open('myvideo.mp4', 'rb')
+    video_bytes = video_file.read()
+
+    st.video(video_bytes)
